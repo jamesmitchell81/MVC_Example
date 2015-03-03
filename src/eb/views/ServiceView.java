6cb0eeb23c8311f6be7754c311ff56d715049091
James Mitchell <james.mitchell81@live.co.uk>
2015-03-03 20:30:46 +0000

    aligned buttons left

--- src/eb/views/ServiceView.java
+++ src/eb/views/ServiceView.java
@@ -8,13 +8,13 @@ import java.awt.*;
 // for a customer request a service.
 public class ServiceView extends JPanel// implements ServiceObserver
 {
-  private JButton oilChange = new JButton("Oil Change");
-  private JButton tyreChange = new JButton("Tyre Change");;
-  private JButton paintRespray = new JButton("Paint Respray");;
-  private JButton mot = new JButton("MOT");;
-  private JButton fullService = new JButton("Full Service");;
-  private JButton replaceExhaust = new JButton("Replace Exhaust");
-  private JLabel display = new JLabel();
+  private JButton oilChange;
+  private JButton tyreChange;
+  private JButton paintRespray;
+  private JButton mot;
+  private JButton fullService;
+  private JButton replaceExhaust;
+  private JLabel display;
 
   private ServiceModel model;
 
@@ -24,6 +24,26 @@ public class ServiceView extends JPanel// implements ServiceObserver
     this.model = sm;
 
     this.setLayout(new GridLayout(7, 0));
+    this.createView();
+  }
+
+  private void createView()
+  {
+
+    this.oilChange = new JButton("Oil Change");
+    this.oilChange.setHorizontalAlignment(SwingConstants.LEFT);
+    this.tyreChange = new JButton("Tyre Change");
+    this.tyreChange.setHorizontalAlignment(SwingConstants.LEFT);
+    this.paintRespray = new JButton("Paint Respray");
+    this.paintRespray.setHorizontalAlignment(SwingConstants.LEFT);
+    this.mot = new JButton("MOT");
+    this.mot.setHorizontalAlignment(SwingConstants.LEFT);
+    this.fullService = new JButton("Full Service");
+    this.fullService.setHorizontalAlignment(SwingConstants.LEFT);
+    this.replaceExhaust = new JButton("Replace Exhaust");
+    this.replaceExhaust.setHorizontalAlignment(SwingConstants.LEFT);
+    this.display = new JLabel();
+    this.display.setHorizontalAlignment(SwingConstants.LEFT);
 
     this.add(oilChange);
     this.add(tyreChange);
@@ -31,7 +51,6 @@ public class ServiceView extends JPanel// implements ServiceObserver
     this.add(mot);
     this.add(fullService);
     this.add(replaceExhaust);
-
     this.add(display);
   }
 }
